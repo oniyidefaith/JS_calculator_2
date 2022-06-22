817c38237bf37cb7e962cd986b059cae5532a846
Oniyide Faith <72167184+oniyidefaith@users.noreply.github.com>
2022-06-22 20:46:24 +0100

    first commit

---
+++ cal.js
@@ -0,0 +1,10 @@
+// making a calculator in js without front end 
+console.log ('numbers are!')
+let first_number = 12
+let second_number = 13 ;
+const sum = first_number + second_number
+const multiplication = first_number * second_number
+const division = first_number / second_number
+const subtraction = first_number - second_number
+ console.log(`first number is ${first_number} \n second number  is ${second_number}\n`)
+ console.log (`addition of two number is ${sum}\n multiplication ${multiplication} \n division is ${division} \n subtraction is ${subtraction}`)
